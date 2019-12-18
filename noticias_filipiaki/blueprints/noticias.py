6e88ecdb5c428f32c296b7a90acb100f3a269b33
Maicon Douglas Filipiaki <mdfilipiaki@gmail.com>
2019-12-18 14:17:00 -0300

    :sparkles: New Arquitetura

--- noticias_filipiaki/blueprints/noticias.py
+++ noticias_filipiaki/blueprints/noticias.py
@@ -1,23 +1,23 @@
-from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
+from flask import Blueprint
 
-app = Flask(__name__)
+noticias_blueprint = Blueprint('noticias', __name__)
 
 
-@app.route('/', methods=['GET'])
+@noticias_blueprint.route('/', methods=['GET'])
 def index():
     return render_template('index.html', title="Noticias Filipiaki")
 
 
-@app.route('/noticia/<int:id>', methods=['GET'])
+@noticias_blueprint.route('/noticia/<int:id>', methods=['GET'])
 def details_news(id):
     return render_template('details.html', id=id)
 
 
-@app.route('/noticia/list', methods=['GET'])
-@app.route('/noticia/register/new', methods=['POST'])
+@noticias_blueprint.route('/noticia/list', methods=['GET'])
+@noticias_blueprint.route('/noticia/register/new', methods=['POST'])
 def register_news():
     if request.method == 'GET':
         return render_template('list.html')
@@ -26,8 +26,8 @@ def register_news():
     return jsonify({'msg': 'ERROR NOT METHOD LIST'}), 503
 
 
-@app.route('/noticia/register/<int:id>', methods=['PUT'])
-@app.route('/noticia/delete/<int:id>', methods=['DELETE'])
+@noticias_blueprint.route('/noticia/register/<int:id>', methods=['PUT'])
+@noticias_blueprint.route('/noticia/delete/<int:id>', methods=['DELETE'])
 def modifications_news(id):
     if request.method == 'PUT':
         return render_template('register.html')
@@ -36,9 +36,7 @@ def modifications_news(id):
     return jsonify({'msg': 'ERROR NOT METHOD LIST'}), 503
 
 
-@app.route('/noticia/register', methods=['GET'])
+@noticias_blueprint.route('/noticia/register', methods=['GET'])
 def form_new_edit_news():
     return render_template('register.html')
 
-
-app.run(debug=True, use_reloader=True)
